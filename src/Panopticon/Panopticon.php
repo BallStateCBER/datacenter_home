d01e328e77cbaa7bbe13dfc78934824814a7e9c8
Graham Watson <graham@phantomwatson.com>
2021-02-17 17:31:42 -0500

    Move repo data to config file

--- src/Panopticon/Panopticon.php
+++ src/Panopticon/Panopticon.php
@@ -8,169 +8,21 @@ use Github\Client;
 class Panopticon
 {
     /**
+     * Panopticon constructor
+     */
+    public function __construct()
+    {
+        Configure::load('repos');
+    }
+
+    /**
      * Returns an array of repo details, indexed by the GitHub repo name
      *
      * @return array
      */
     public function getSiteDetails()
     {
-        return [
-            'brownfield' => [
-                'title' => 'Brownfield Grant Writers\' Tool',
-                'production' => 'https://brownfield.cberdata.org',
-                'development' => 'https://brownfield.localhost/'
-            ],
-            'brownfields-updater' => [
-                'title' => 'Brownfield Grant Writers\' Tool Data Importer'
-            ],
-            'cber-data-grabber' => [
-                'title' => 'CBER Data Grabber'
-            ],
-            'commentaries' => [
-                'title' => 'Weekly Commentaries',
-                'production' => 'https://commentaries.cberdata.org',
-                'development' => 'https://commentaries.localhost'
-            ],
-            'commentaries-cake3' => [
-                'title' => 'Weekly Commentaries (CakePHP 3)',
-                'production' => 'https://cake3.commentaries.cberdata.org'
-            ],
-            'community-asset-inventory' => [
-                'title' => 'Community Asset Inventory (CakePHP 2)',
-                'development' => 'https://qop.localhost'
-            ],
-            'community-asset-inventory-cakephp3' => [
-                'title' => 'Community Asset Inventory (CakePHP 3)',
-                'production' => 'https://cair.cberdata.org',
-                'staging' => 'https://staging.cair.cberdata.org',
-                'development' => 'https://asset.localhost'
-            ],
-            'conexus' => [
-                'title' => 'Manufacturing Score Card (formerly Conexus)',
-                'production' => 'https://mfgscorecard.cberdata.org',
-                'development' => 'https://mfgscorecard.localhost'
-            ],
-            'county-profiles' => [
-                'title' => 'County Profiles',
-                'production' => 'https://profiles.cberdata.org',
-                'development' => 'https://profiles.localhost'
-            ],
-            'county-profiles-updater' => [
-                'title' => 'County Profiles Updater'
-            ],
-            'datacenter-home' => [
-                'title' => 'CBER Data Center Home (CakePHP 3)',
-                'production' => 'https://cberdata.org',
-                'staging' => 'https://staging.home.cberdata.org',
-                'development' => 'https://dchome3.localhost'
-            ],
-            'datacenter-skeleton' => [
-                'title' => 'CBER Data Center Website Skeleton'
-            ],
-            'datacenter-plugin-cakephp3' => [
-                'title' => 'Data Center Plugin (CakePHP 3)'
-            ],
-            'deploy' => [
-                'title' => 'CBER Deploy-bot',
-                'production' => 'https://deploy.cberdata.org',
-                'development' => 'https://deploy.localhost'
-            ],
-            'economic-indicators' => [
-                'title' => 'Economic Indicators (CakePHP 2)',
-                'production' => 'https://indicators.cberdata.org',
-                'development' => 'https://indicators.localhost'
-            ],
-            'economic-indicators-cakephp4' => [
-                'title' => 'Economic Indicators (CakePHP 4)',
-                'production' => 'https://indicators4.cberdata.org',
-                'development' => 'https://indicators4.localhost'
-            ],
-            'honest-pledge' => [
-                'title' => 'Honest Muncie Pledge',
-                'production' => 'https://pledge.honestmuncie.org',
-                'development' => 'https://honestpledge.localhost'
-            ],
-            'ice-miller-cakephp3' => [
-                'title' => 'Ice Miller / EDGE Articles',
-                'production' => 'https://icemiller.cberdata.org',
-                'development' => 'https://icemiller3.localhost'
-            ],
-            'mfg-scr-crd' => [
-                'title' => 'Manufacturing Scorecard'
-            ],
-            'muncie-musicfest2' => [
-                'title' => 'Muncie MusicFest (CakePHP 3)',
-                'production' => 'https://munciemusicfest.com',
-                'development' => 'https://mmf.localhost'
-            ],
-            'muncie-events' => [
-                'title' => 'Muncie Events (CakePHP 2)',
-                'production' => 'https://muncieevents.com',
-                'staging' => 'https://staging.muncieevents.com',
-                'development' => 'https://muncie-events.localhost'
-            ],
-            'muncie-events3' => [
-                'title' => 'Muncie Events (CakePHP 3)',
-                'production' => 'https://cake3.muncieevents.com',
-                'staging' => 'https://staging.cake3.muncieevents.com',
-                'development' => 'https://muncieevents3.localhost'
-            ],
-            'muncie-events-api' => [
-                'title' => 'Muncie Events API',
-                'production' => 'https://api.muncieevents.com',
-                'staging' => 'https://staging.api.muncieevents.com',
-                'development' => 'https://meapi.localhost'
-            ],
-            'notes' => [
-                'title' => 'CBER Web Development Notes'
-            ],
-            'projects' => [
-                'title' => 'CBER Projects and Publications (CakePHP 2)',
-                'production' => 'https://projects.cberdata.org',
-                'development' => 'https://projects.localhost'
-            ],
-            'projects-cakephp3' => [
-                'title' => 'CBER Projects and Publications (CakePHP 3)'
-            ],
-            'projects-cakephp4' => [
-                'title' => 'CBER Projects and Publications (CakePHP 4)',
-                'production' => 'https://beta.projects.cberdata.org',
-                'staging' => 'https://staging.projects.cberdata.org',
-                'development' => 'https://projects4.localhost'
-            ],
-            'roundtable' => [
-                'title' => 'BSU Roundtable (CakePHP 2)'
-            ],
-            'datacenter-plugin' => [
-                'title' => 'Data Center Plugin (CakePHP 2)'
-            ],
-            'datacenter-plugin-cakephp4' => [
-                'title' => 'Data Center Plugin (CakePHP 4)'
-            ],
-            'data-center-template' => [
-                'title' => 'Data Center Template'
-            ],
-            'GoogleCharts' => [
-                'title' => 'Google Charts Plugin for CakePHP (fork)'
-            ],
-            'utilities' => [
-                'title' => 'CBER Utilities'
-            ],
-            'student-work' => [
-                'title' => 'Student Work Tracker',
-                'production' => 'https://studentwork.cberdata.org',
-                'staging' => 'https://staging.studentwork.cberdata.org'
-            ],
-            'school-rankings' => [
-                'title' => 'School Rankings',
-                'production' => 'https://school.cberdata.org',
-            ],
-            'vore-arts-fund' => [
-                'title' => 'Vore Arts Fund',
-                'production' => 'https://voreartsfund.org',
-                'staging' => 'https://staging.voreartsfund.org',
-            ]
-        ];
+        return Configure::read('repos');
     }
 
     /**
