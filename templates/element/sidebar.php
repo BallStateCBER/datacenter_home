6e827d4d9703a75a6e0c94d5e327c8e1c5125867
Graham Watson <graham@phantomwatson.com>
2021-02-17 16:05:29 -0500

    Update Twitter widget

--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -44,6 +44,6 @@ if ($releaseIsValid) {
     <h1 class="sr-only">
         <a href="https://twitter.com/BallStateCBER">@BallStateCBER</a>
     </h1>
-    <a class="twitter-timeline"  href="https://twitter.com/BallStateCBER"  data-widget-id="351709426740252672">Tweets by @BallStateCBER</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    <a class="twitter-timeline" href="https://twitter.com/BallStateCBER?ref_src=twsrc%5Etfw">Tweets by BallStateCBER</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </section>
