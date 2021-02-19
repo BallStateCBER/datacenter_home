c36b45a3460d03da623c3915a3bec3269ed37ea3
Graham Watson <graham@phantomwatson.com>
2021-02-18 19:34:12 -0500

    Make Twitter feed responsive

--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -40,10 +40,25 @@ if ($releaseIsValid) {
     </section>
 <?php endif; ?>
 
-<section id="twitter">
-    <h1 class="sr-only">
-        <a href="https://twitter.com/BallStateCBER">@BallStateCBER</a>
-    </h1>
-    <a class="twitter-timeline" href="https://twitter.com/BallStateCBER?ref_src=twsrc%5Etfw">Tweets by BallStateCBER</a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<section class="navbar-light navbar-expand-md sidebar-collapse">
+    <div class="row d-md-none">
+        <div class="col">
+            <h2 data-toggle="collapse" data-target="#twitter">
+                Tweets by @BallStateCBER
+            </h2>
+        </div>
+        <div class="col-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#twitter"
+                    aria-controls="tags-list" aria-expanded="false" aria-label="Toggle Twitter feed">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <section id="twitter" class="collapse navbar-collapse">
+        <h1 class="sr-only">
+            <a href="https://twitter.com/BallStateCBER">@BallStateCBER</a>
+        </h1>
+        <a class="twitter-timeline" href="https://twitter.com/BallStateCBER?ref_src=twsrc%5Etfw">Tweets by BallStateCBER</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </section>
 </section>
